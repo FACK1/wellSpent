1cd4217b8cbc689b9810a5c469ed313330d64acd
safaaamro <safaaamro072@gmail.com>
2019-03-20 11:22:59 +0200

    file structure #28

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 require("env2")(".env");
 
-app.listen(process.env.PORT || 3000);
-console.log("PORT WWORK IN 3000");
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "public", "index.html"));
+});
+
+app.listen(process.env.PORT || 5000);
+console.log("PORT WWORK IN 5000");
