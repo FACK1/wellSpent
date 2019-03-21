0da6389a73115393b41d9b445183aa96fb9cd054
samaamro20 <samaamro20@gmail.com>
2019-03-21 10:30:58 +0200

    update header #34

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from './components/Header'
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Header />
-      
-          <h1>Hello Buty Girls ^_^</h1>
-
       </div>
     );
   }
