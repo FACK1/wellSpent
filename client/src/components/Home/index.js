5f6c6c7b95336f9b60968222ab7af19335468d9e
samaamro20 <samaamro20@gmail.com>
2019-03-21 14:46:34 +0200

    clean code #31

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,11 +2,6 @@ import React, {Component} from 'react';
 import './home.css';
 
 export default class Home extends Component {
-
-  constructor(props) {
-      super(props);
-    }
-
   render() {
     return (
       <div className='main'>
