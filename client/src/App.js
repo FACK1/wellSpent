ab7f9dd3c544d389e1e46524fb430bab9f2c8a2f
shaima96 <141062@ppu.edu.ps>
2019-03-22 03:14:43 -0500

    remove import

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
-import Brand from "./components/Brand";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Brand />
       </div>
     );
   }
