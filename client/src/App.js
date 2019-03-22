550fad88348e3910c44b8212bfd86ffb7612a780
shaima96 <141062@ppu.edu.ps>
2019-03-21 23:19:02 -0500

    create brand component #33

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import "./App.css";
+import Brand from "./components/Brand";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Hello Buty Girls ^_^</h1>
-        </header>
+        <Brand />
       </div>
     );
   }
