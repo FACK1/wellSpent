885c26365abe9863631943b1915d6f863c2e5ba7
safaaamro <safaaamro072@gmail.com>
2019-03-24 12:29:31 +0200

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Brands from "./components/Brands";
 import Header from "./components/Header";
@@ -7,10 +8,13 @@ import Home from "./components/Home";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Brands />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/Brands" component={Brands} />
+        </div>
+      </Router>
     );
   }
 }
--- client/src/components/Brands/index.js
+++ client/src/components/Brands/index.js
@@ -1,61 +1,86 @@
 import React, { Component } from "react";
 import "./brands.css";
-import addis from "./addis.png";
+// import axios from "axios";
 
-//
-// class Brands extends Component {
-//   state = {
-//     error: null
-//   };
+class Brands extends Component {
+  state = {
+    loading: false
+  };
 
-export default class Home extends Component {
+  // componentDidMount() {
+  //   axios
+  //     .get("/brands")
+  //     .then(({ data }) => {
+  //       const brands = data.map(brands => {
+  //         return {
+  //           id: brands.id,
+  //           image_url: brands.imge,
+  //           name: brands.name
+  //         };
+  //       });
+  //       this.setState({
+  //         brands,
+  //         loading: true
+  //       });
+  //     })
+  //     .catch(() => {
+  //       const { history } = this.props;
+  //       history.push("/error");
+  //     });
+  // }
+
   render() {
-    return (
-      <div>
-        <div className="div1">
-          <div className="div10">
-            <div className="back" />
-            <div className="par">Ethical Brands</div>
+    if (this.state.loading === true) {
+      let Brands = this.state.brands;
+      return (
+        <div>
+          <div className="div1">
+            <div className="div10">
+              <div className="back" />
+              <div className="par">Ethical Brands</div>
+            </div>
+            <div className="div11">
+              <div className="add" />
+              <div className="par2"> Suggest a brand </div>
+            </div>
           </div>
-          <div className="div11">
-            <div className="add" />
-            <div className="par2"> Suggest a brand </div>
+          <div className="diva">
+            <div className="par3">A</div>
           </div>
-        </div>
-        <div className="diva">
-          <div className="par3">A</div>
-        </div>
 
-        <div className="div-box">
-          <div className="imge" />
-          <div className="descrip">
-            <p className="name">ADDIS</p>
-            <p className="description">
-              {" "}
-              Adidas SA is a Spanish fast fashion retailer based in Arteixo in
-              Galicia.
-            </p>
-            <button className="view">view</button>
-          </div>
-          <div className="scrol">
-            <div className="scrol1">
-              {" "}
-              95 <br />
-              Overall score
+          <div className="div-box">
+            <div className="imge" />
+            <div className="descrip">
+              <p className="name">ADDIS</p>
+              <p className="description">
+                {" "}
+                Adidas SA is a Spanish fast fashion retailer based in Arteixo in
+                Galicia.
+              </p>
+              <button className="view">view</button>
             </div>
-            <div className="scrol2">
-              {" "}
-              70 <br />
-              Labour score
-            </div>
-            <div className="scrol3">
-              {" "}
-              95 <br />
-              Enviroment score
+            <div className="scrol">
+              <div className="scrol1">
+                {" "}
+                95 <br />
+                Overall score
+              </div>
+              <div className="scrol2">
+                {" "}
+                70 <br />
+                Labour score
+              </div>
+              <div className="scrol3">
+                {" "}
+                95 <br />
+                Enviroment score
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
+
+export default Brands;
