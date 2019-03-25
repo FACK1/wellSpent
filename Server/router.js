d3d1948c5a283703aa8b953b04b81fdba889e1c7
shaima96 <141062@ppu.edu.ps>
2019-03-24 23:05:11 -0500

    create server file #40

--- Server/router.js
+++ Server/router.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { brand } = require('./controllers/brand.js');
+const { getBrand } = require('./controllers/brand.js');
 
 const router = express.Router();
 
-router.get('/brand/:id', brand);
+router.get('/brand/:id', getBrand);
 module.exports = router;
