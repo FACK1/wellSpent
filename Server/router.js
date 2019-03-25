a83a54f109b3b9bbef73c3324a91ddd135907091
safaaamro <safaaamro072@gmail.com>
2019-03-25 12:09:20 +0200

    delete some file #45

--- Server/router.js
+++ Server/router.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const { getBrand } = require('./controllers/brand.js');
-const { getBrands } = require('./controllers/brands.js');
 
 const router = express.Router();
 
 router.get('/brand/:id', getBrand);
-router.get('/brands', getBrands);
+
 module.exports = router;
