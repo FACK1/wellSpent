935a8914626f6aa7cd3f874c8f989c1f0132e131
shaima96 <141062@ppu.edu.ps>
2019-03-24 23:07:11 -0500

    change the type of font in css , get the data using axios#40

--- client/src/components/Brand/index.js
+++ client/src/components/Brand/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import imageback from "./image.png";
-import brand from "./Rectangle.png";
 import "./brand.css";
+import image from "./Rectangle.png";
 import axios from "axios";
 
 class Brand extends React.Component {
+  state = {
+    loading: false
+  };
   componentDidMount() {
     const id = 1;
     axios
       .get(`/brand/${id}`)
       .then(({ data }) => {
         this.setState({
-          image: data.image,
           name: data.name,
-          OverallScore: data.OverallScore,
-          EnviormentScore: data.EnviormentScore,
-          AnimalRightScore: data.AnimalRightScore,
-          Cost: data.Cost,
-          ProductType: data.ProductType,
-          Explanation: data.Explanation
+          overallscore: data.overallscore,
+          enviormentScore: data.enviormentscore,
+          labourscore: data.labourscore,
+          animalrightscore: data.animalrightscore,
+          cost: data.cost,
+          producttype: data.producttype,
+          explanation: data.explanation,
+          loading: true
         });
       })
       .catch(err => {
@@ -27,53 +31,67 @@ class Brand extends React.Component {
   }
 
   render() {
-    return (
-      <div className="root">
-        <div className="container">
-          <img className="back" src={imageback} alt="img" />
-          <p className="pargraph"> Brand Details </p>
-        </div>
-        <div className="image">
-          <img className="images" src={brand} alt="img" />
-        </div>
-        <div className="name">
-          <h3>Zara</h3>
-        </div>
-        <div className="details" />
-        <div className="overall">
-          <div className="score">
-            <h3>95 </h3>
-            <h3>OverallScore </h3>
+    const {
+      name,
+      overallScore,
+      labourScore,
+      loading,
+      enviormentscore,
+      animalrightscore,
+      cost,
+      producttype,
+      explanation
+    } = this.state;
+    if (loading) {
+      return (
+        <div className="root">
+          <div className="container">
+            <img className="back" src={imageback} alt="img" />
+            <p className="pargraph"> Brand Details </p>
           </div>
-        </div>
-        <div className="detail">
-          <div className="labour">
-            <div className="score1">
-              <h3>100 LabourScore</h3>
+          <div className="image">
+            <img className="images" src={image} alt="img" />
+          </div>
+          <div className="name">
+            <h3>{name}</h3>
+          </div>
+          <div className="details" />
+          <div className="overall">
+            <div className="score">
+              <h3>OverallScore</h3>
+              <h3>{overallScore} </h3>
             </div>
           </div>
-          <div className="enviorment">
-            <div className="score2">
-              <h3>88 EnviormentScore</h3>
+          <div className="detail">
+            <div className="labour">
+              <div className="score1">
+                <h3>LabourScore</h3>
+                <h3>{labourScore}</h3>
+              </div>
+            </div>
+            <div className="enviorment">
+              <div className="score2">
+                <h3>EnviormentScore</h3>
+                <h3>{enviormentscore}</h3>
+              </div>
+            </div>
+            <div className="animal">
+              <div className="score3">
+                <h3>AnimalRightScore</h3>
+                <h3>{animalrightscore}</h3>
+              </div>
             </div>
           </div>
-          <div className="animal">
-            <div className="score3">
-              <h3>70 AnimalRightScore</h3>
-            </div>
+          <div className="more">
+            <h3> Cost:{cost}</h3>
+            <h3> ProductType:{producttype}</h3>
+            <h3>Explanation :{explanation}</h3>
           </div>
         </div>
-        <div className="more">
-          <h3> Cost:10$</h3>
-          <h3> ProductType:Socided Anonima</h3>
-          <h3>
-            Explanation : We looked at Zara's score in the TearFund report
-            <br /> and the got a ranking of A+ for transparency,
-            <br /> which is why we gave it a score of 80 for Labour policy.
-          </h3>
-        </div>
-      </div>
-    );
+      );
+    } else {
+      return <div>loading </div>;
+    }
   }
 }
 
