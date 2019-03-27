849210dfdb82867d087c2fb6d0b2167825e4937c
shaima96 <141062@ppu.edu.ps>
2019-03-27 01:26:09 -0500

    Remove unrequired route and get data about brand from database using name brand #44

--- Server/controllers/brand.js
+++ Server/controllers/brand.js
@@ -2,20 +2,17 @@ const Airtable = require('airtable');
 
 exports.getBrand = (req, res) => {
   const { name } = req.params;
-  console.log('name', name);
   const base = new Airtable({ apiKey: 'keyYymTkZ8tlYgUmY' }).base('appNifWvpBhZkgaLk');
-
   base('Brands')
     .select({
-      filterByFormula: `{Name}=${name}`,
+      view: 'API Response',
+      filterByFormula: `{Name}="${name}"`,
     })
     .eachPage(
       (records, fetchNextPage) => {
-        records.forEach((record) => {
-          console.log('Retrieved', record);
-        });
-        console.log('shaima');
+        const result = records.map(record => record.fields);
         fetchNextPage();
+        res.json(result);
       },
       (error) => {
         console.log(error);
