23a8cbc726c6ceba1b1c7fc851f19349865d22f8
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-03-31 09:18:44 +0300

    update

--- client/src/components/Brands/index.js
+++ client/src/components/Brands/index.js
@@ -65,75 +65,82 @@ class Brands extends Component {
               <div className="par2"> Suggest a brand </div>
             </div>
           </div>
-
-          {brands.length !== 0 ? (
-            brands.map(brand => {
-              return (
+          {letters.length !== 0 ? (
+            letters.map((letter,index) => {
+              if(letter)
+              return(
                 <div>
-                  {letters.length !== 0 ? (
-                    letters.map(letter => {
-                      if (letter === brand.Name.charAt(0)) {
-                        return (
-                          <div className="diva">
-                            <div className="par3">{letter}</div>
-                          </div>
-                        );
-                      }
-                    })
-                  ) : (
-                    <div> </div>
-                  )}
-                  <div className="div-box">
-                    <div className="imge">
-                      <img
-                        className="img-get"
-                        src={brand.Image[0].thumbnails.large.url}
-                        alt=""
-                      />
-                    </div>
-                    <div className="descrip">
-                      <p className="name">{brand.Name}</p>
-                      <p className="description">{brand.Explanation}</p>
-                      <Link to={`/Brand/${brand.Name}`} className="button-link">
-                        <button className="view">view</button>
-                      </Link>
-                    </div>
-                    <div className="scrol">
-                      <div
-                        className="scrol1"
-                        style={{
-                          "background-color": `${brand.OverallScoreColour[0]}`
-                        }}
-                      >
-                        {brand.OverallScore} <br /> overall score
-                      </div>
-                      <div
-                        className="scrol2"
-                        style={{
-                          "background-color": `${brand.LabourScoreColour[0]}`
-                        }}
-                      >
-                        {brand.LaborScore} <br /> labour score
-                      </div>
-                      <div
-                        className="scrol3"
-                        style={{
-                          "background-color": `${
-                            brand.EnvironmentScoreColour[0]
-                          }`
-                        }}
-                      >
-                        {brand.EnvironmentScore} <br /> enviorment score
-                      </div>
-                    </div>
+                  <div className="diva">
+                    <div className="par3">{letter}</div>
                   </div>
+
+                { brands.length !== 0 ? (
+                  brands.map((brand, i) => {
+                    if (letter === brand.Name.charAt(0) ) {
+                    return (
+                      <div>
+
+                        <div className="div-box">
+                          <div className="imge">
+                            <img
+                              className="img-get"
+                              src={brand.Image[0].thumbnails.large.url}
+                              alt=""
+                            />
+                          </div>
+                          <div className="descrip">
+                            <p className="name">{brand.Name}</p>
+                            <p className="description">{brand.Explanation}</p>
+                            <Link to={`/Brand/${brand.Name}`} className="button-link">
+                              <button className="view">view</button>
+                            </Link>
+                          </div>
+                          <div className="scrol">
+                            <div
+                              className="scrol1"
+                              style={{
+                                "background-color": `${brand.OverallScoreColour[0]}`
+                              }}
+                            >
+                              {brand.OverallScore} <br /> overall score
+                            </div>
+                            <div
+                              className="scrol2"
+                              style={{
+                                "background-color": `${brand.LabourScoreColour[0]}`
+                              }}
+                            >
+                              {brand.LaborScore} <br /> labour score
+                            </div>
+                            <div
+                              className="scrol3"
+                              style={{
+                                "background-color": `${
+                                  brand.EnvironmentScoreColour[0]
+                                }`
+                              }}
+                            >
+                              {brand.EnvironmentScore} <br /> enviorment score
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                    );
+                  }
+                  })
+                ) : (
+
+                  <RingLoader color={"#ff8094"} loading={loading} />
+                )}
                 </div>
-              
-              );
+
+)
             })
           ) : (
-            <RingLoader color={"#ff8094"} loading={loading} />
+            <div> </div>
           )}
+
         </div>
       );
     } else {
