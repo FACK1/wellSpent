84f4989382bd634cfa2f1cb0fabd6c62585266f1
shaima96 <141062@ppu.edu.ps>
2019-04-01 10:07:49 -0500

    fix conflict

--- client/src/components/Feedback/index.js
+++ client/src/components/Feedback/index.js
@@ -29,50 +29,52 @@ export default class Feedback extends Component {
   }
   render() {
     return (
-      <div className="mainf">
-        <div className="feedback">
-          <div className="header">
-            <img src={lamba} alt="lamba" />
-            <h3>What does the community think about Zara </h3>
-          </div>
-          <div className="mainorg">
-            <div className="line" />
-            <div className="cards">
-              <h3>Feedback</h3>
-              {this.state.info.length !== 0 ? (
-                this.state.info.map(i => {
-                  return (
-                    <div className="allcards">
-                      <div className="carduser">
-                        <div className="feedbackcard">
-                          <img src={user} alt="user" />
-                          <div className="nameuser">
-                            <h4>Name</h4>
-                            <h5>{i.Name}</h5>
+      <div className="maina">
+        <div className="mainf">
+          <div className="feedback">
+            <div className="header">
+              <img src={lamba} alt="lamba" />
+              <h3>What does the community think about Zara </h3>
+            </div>
+            <div className="mainorg">
+              <div className="line" />
+              <div className="cards">
+                <h3>Feedback</h3>
+                {this.state.info.length !== 0 ? (
+                  this.state.info.map(i => {
+                    return (
+                      <div className="allcards">
+                        <div className="carduser">
+                          <div className="feedbackcard">
+                            <img src={user} alt="user" />
+                            <div className="nameuser">
+                              <h4>Name</h4>
+                              <h5>{i.Name}</h5>
+                            </div>
+                            <div className="feedback">
+                              <h4>Feedback</h4>
+                              <h5>{i.feedback}</h5>
+                            </div>
                           </div>
-                          <div className="feedback">
-                            <h4>Feedback</h4>
-                            <h5>{i.feedback}</h5>
+                          <div className="votes">
+                            <div className="like">
+                              <img src={like} alt="like" />
+                              <p>{i.like}</p>
+                            </div>
+                            <div className="dislike">
+                              <img src={dislike} alt="dislike" />
+                              <p>{i.dislike}</p>
+                            </div>
                           </div>
+                          <div className="line2" />
                         </div>
-                        <div className="votes">
-                          <div className="like">
-                            <img src={like} alt="like" />
-                            <p>{i.like}</p>
-                          </div>
-                          <div className="dislike">
-                            <img src={dislike} alt="dislike" />
-                            <p>{i.dislike}</p>
-                          </div>
-                        </div>
-                        <div className="line2" />
                       </div>
-                    </div>
-                  );
-                })
-              ) : (
-                <RingLoader color={"#ff8094"} loading={this.state.loading} />
-              )}
+                    );
+                  })
+                ) : (
+                  <RingLoader color={"#ff8094"} loading={this.state.loading} />
+                )}
+              </div>
             </div>
           </div>
         </div>
