0b7b3e1fc33a9a601d156ae1960cc0b1e6df01bf
samaamro20 <samaamro20@gmail.com>
2019-04-02 14:45:19 +0300

    vote to feedback #93

--- Server/router.js
+++ Server/router.js
@@ -5,6 +5,9 @@ const { getAboutus } = require('./controllers/aboutus.js');
 const { addBrand } = require('./controllers/addBrand.js');
 const { getfeedback } = require('./controllers/getfeedback.js');
 const { addfeedback } = require('./controllers/addfeedback.js');
+const { like } = require('./controllers/like.js');
+const { dislike } = require('./controllers/dislike.js');
+
 
 const router = express.Router();
 
@@ -14,5 +17,8 @@ router.get('/aboutus', getAboutus);
 router.post('/brand', addBrand);
 router.get('/getfeedback/:name', getfeedback);
 router.post('/addfeedback', addfeedback);
+router.get('/like/:id/:like', like);
+router.get('/dislike/:id/:dislike', dislike);
+
 
 module.exports = router;
