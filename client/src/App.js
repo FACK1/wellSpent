eab43a2501f548d2838f6f201f58ea7db2b214c8
Shahenaz Monia <shahenazmonia@Shahenazs-MacBook-Pro.local>
2019-04-03 16:33:24 +0300

    use switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -10,14 +10,14 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-      <Router>
+      <Switch>
         <div className="App">
           <Header />
           <Route exact path="/" component={Home} />
           <Route exact path="/Brands" component={Brands} />
           <Route exact path="/Brand/:name" component={Brand} />
         </div>
-      </Router>
+      </Switch>
       </HashRouter>
     );
   }
