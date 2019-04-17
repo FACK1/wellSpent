c4037411cbd21647b021024daeaa72827f901673
samaamro20 <samaamro20@gmail.com>
2019-04-17 11:02:58 +0300

    Done

--- client/src/components/Brand/index.js
+++ client/src/components/Brand/index.js
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import defaultimage from "./x.jpg";
 import AddFeedback from "../Feedback/add.js";
 
+
 class Brand extends React.Component {
   state = {
     info: [],
